90ddda2d7cb4d81f2d05b6aea5484b4c33f904d7
RehanProject <rehan27tifanno@gmail.com>
2025-01-24 00:57:53 +0700

    first commit lagi

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Home from './Home'
-import Keranjang from './Keranjang'
-import Booking from './Booking'
-import JanjiTemu from './JanjiTemu'
-import Profile from './Profile'
-
-export { Booking, Home, JanjiTemu, Keranjang, Profile }
