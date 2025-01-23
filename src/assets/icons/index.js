5a1c1f15b7bf2e106222eebb2a634dbbb6ef3509
RehanProject <rehan27tifanno@gmail.com>
2025-01-23 19:56:13 +0700

    first commit lagi

--- src/assets/icons/index.js
+++
@@ -1,6 +0,0 @@
-import IconBaterry from './Baterry.svg';
-import IconClock from './Clock.svg';
-import IconConnection from './Connection.svg';
-import IconSignal from './Signal.svg';
-
-export { IconBaterry, IconClock, IconConnection, IconSignal };
